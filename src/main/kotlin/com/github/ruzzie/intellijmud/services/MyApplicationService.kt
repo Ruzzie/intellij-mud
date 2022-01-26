f595de007d18937391958978a44194827dfb2578
GitHub Action <action@github.com>
2022-01-26 14:12:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/ruzzie/intellijmud/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ruzzie/intellijmud/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ruzzie.intellijmud.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ruzzie.intellijmud.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ruzzie/intellijmud/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ruzzie/intellijmud/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ruzzie.intellijmud.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ruzzie.intellijmud.MyBundle
 
 class MyProjectService(project: Project) {
 
